a6ab48d311e4bba5d21ce941bef5d21c9dde2495
dylanjshaw <dylanjshaw@sandiego.edu>
2017-04-03 21:37:12 -0700

    Add bootstrap gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.react.variant = :development
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -51,8 +53,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-end
-
-MyApp::Application.configure do
-  config.react.variant = :development
 end
